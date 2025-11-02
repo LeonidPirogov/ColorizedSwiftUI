4e6f648da9db4ce536acad8ec00fdf6aecf85c30
LeonidPirogov <lv.pirogof@gmail.com>
2025-11-02 13:04:15 +0300

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Leonid on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
