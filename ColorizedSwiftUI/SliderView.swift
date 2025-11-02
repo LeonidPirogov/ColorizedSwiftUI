2596425b4bc50b8f49e4418182ed5bbde4e9f72d
LeonidPirogov <lv.pirogof@gmail.com>
2025-11-02 15:12:43 +0300

    Update project settings: iPhone only, Portrait mode

---
+++ ColorizedSwiftUI/SliderView.swift
@@ -0,0 +1,18 @@
+//
+//  SliderView.swift
+//  ColorizedSwiftUI
+//
+//  Created by Leonid on 02.11.2025.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderView()
+}
