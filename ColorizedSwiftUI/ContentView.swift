e1fb1d691c1ec488218c66fd18155a015c0032ef
LeonidPirogov <lv.pirogof@gmail.com>
2025-11-04 13:16:05 +0300

    feat(swiftui): implement ContentView as root layout with sliders and color preview

--- ColorizedSwiftUI/ContentView.swift
+++ ColorizedSwiftUI/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var red = Double.random(in: 0...255).rounded()
+    @State private var green = Double.random(in: 0...255).rounded()
+    @State private var blue = Double.random(in: 0...255).rounded()
+    
+    @FocusState private var isInputActive: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 40) {
+                ColorView(red: red, green: green, blue: blue)
+                
+                VStack {
+                    ColorSliderView(value: $red, color: .red)
+                    ColorSliderView(value: $green, color: .green)
+                    ColorSliderView(value: $blue, color: .blue)
+                }
+                .frame(height: 150)
+                .focused($isInputActive)
+                
+                Spacer()
+            }
         }
         .padding()
+        .background(Color.background)
+        .onTapGesture {
+            isInputActive = false
+        }
+        
+        .safeAreaInset(edge: .bottom) {
+            if isInputActive {
+                HStack {
+                    Spacer()
+                    Button("Done") { isInputActive = false }
+                        .buttonStyle(.borderedProminent)
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 8)
+            } else {
+                EmptyView()
+            }
+        }
     }
 }
 
